29d2a874e745efaaf0ccfdfb79378d165be76a16
Michal Fuhrmann <michal.fuhrmann.w@gmail.com>
2020-09-13 08:25:34 +0100

    Initial commit

---
+++ src/main/java/com/mfuhrmann/assignment/OrderProductAppApplication.java
@@ -0,0 +1,13 @@
+package com.mfuhrmann.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderProductAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderProductAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mfuhrmann/assignment/OrderProductAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mfuhrmann.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderProductAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
